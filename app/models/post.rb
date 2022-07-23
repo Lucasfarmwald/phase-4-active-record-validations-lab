568b1b8285e8495fbe1e7480b2886df6639f8a29
lucasfarmwald <farmwaldlucas@gmail.com>
2022-07-23 11:31:57 -0500

    commit tree a99260351a2f1a55446d1ac71c243d12ccec9c6c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,23 @@
 class Post < ApplicationRecord
-
+   
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
-    validate :title_is_clickbait?
+    validate  :clickbait?
 
-    def title_is_clickbait?
-        if title.match?('True Facts')
-            
-        else
-            errors.add(:title,"must be clickbait")
+    Clickbait_array = [
+       /Won't Believe/,
+       /Secret/,
+       /Top [number]/,
+       /Guess/
+      ]
+    
+      def clickbait?
+        
+        if Clickbait_array.none? { |arr| arr.match title }
+          errors.add(:title, "must be clickbait")
         end
-    end
-
-   
+      end
 
 end
